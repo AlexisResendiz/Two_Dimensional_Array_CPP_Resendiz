261137976765f4fc61646a5bcacf8696ebb0ffa2
Alexis Resendiz <alexis.resendiz@my.tccd.edu>
2020-04-11 02:48:46 +0000

    added more data to the table in the comments

--- main.cpp
+++ main.cpp
@@ -5,40 +5,40 @@
 #include <iostream>
 using namespace std;
 
-//New_York = 1,Oakland = 2  Replace City_One and City_Two with the names of your cities
+//New_York = 1, Oakland = 2
 
 //Data Table
 
 //City X, Day X: Temp
 /*
 
-City 1, Day 1: 56
+New_York, Day 1: 56
 
-City 1, Day 2: 59
+New_York, Day 2: 59
 
-City 1, Day 3: 68
+New_York, Day 3: 68
 
-City 1, Day 4: 59
+New_York, Day 4: 59
 
-City 1, Day 5: 47
+New_York, Day 5: 47
 
-City 1, Day 6: 53
+New_York, Day 6: 53
 
-City 1, Day 7: 53
+New_York, Day 7: 53
 
-City 2, Day 1: 59
+Oakland, Day 1: 59
 
-City 2, Day 2: 59
+Oakland, Day 2: 59
 
-City 2, Day 3: 43
+Oakland, Day 3: 43
 
-City 2, Day 4: 42
+Oakland, Day 4: 42
 
-City 2, Day 5: 39
+Oakland, Day 5: 39
 
-City 2, Day 6: 45
+Oakland, Day 6: 45
 
-City 2, Day 7: 47
+Oakland, Day 7: 47
 
 */
 
@@ -47,10 +47,13 @@ int main()
       const int CITY = 2;
       const int WEEK = 7;
     int temperature[CITY][WEEK];
+
     //Note your input data from the above
+
     cout << "Enter all temperature for a week of first city and then second city. \n";
 
     // Inserting the values into the temperature array
+
     //note for every dimension of the array you need a for loop 2 dimensions = 2 for loops
 
     for (int i = 0; i < CITY; ++i)
